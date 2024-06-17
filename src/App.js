bdde43d6a2aadb0d6e308d5bd87f634918981095
Harshit Gupta <149142309+harshitgupta240197@users.noreply.github.com>
2024-06-17 22:46:41 +0530

    Updated packages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import {Banner} from './Components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './Components/NavBar';
 import { Skills } from './Components/Skills';
-
-
+import { Projects } from './Components/Projects';
 function App() {
   return (
     <div className="App">
